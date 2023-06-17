a202618580fb4df8fa4d779781ad5489b201f71f
Denis Stoica <denis.stoica@takeofflabs.com>
2023-06-17 20:35:42 +0300

    dfasfas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::API
 
     response = firebase.push("ElderTrack/consultations", {
       "doctorUID": params["extension"][0]["valueReference"]["reference"],
+      "patientUID": params["subject"]["reference"],
+      "description": params["extension"][0]["valueString"],
+      "consultationCode": params["modifierExtension"][0]["valueCodeableConcept"]["coding"][0]["code"],
+      "created_at": Time.zone.now.strftime("%d/%m/%Y").to_s,
       "result": "-"
     })
     render json: {}, status: :ok
