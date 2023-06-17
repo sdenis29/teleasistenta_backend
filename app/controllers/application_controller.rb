d49d00214811b9547a1798943f3554a3cd1e6770
Denis Stoica <denis.stoica@takeofflabs.com>
2023-06-17 20:20:28 +0300

    Add firebase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   def send_consultation
-    render json: {
-      message: "TEST"
-    }
+    firebase_url    = 'https://teleasistenta-83ccd-default-rtdb.europe-west1.firebasedatabase.app'
+    firebase_secret = 'FGxv1Nr6jsNXpZJhXRilP7h3L8WU8E7iz0iGnGko'
+    firebase = Firebase::Client.new(firebase_url, firebase_secret)
+
+    response = firebase.push("ElderTrack/consultations", {
+      "#{params["id"]}": {
+        "result": "sfsafasfas"
+      }
+    })
+    render json: {}, status: :ok
   end
 end
